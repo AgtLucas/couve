d5a0ee75ddf0c0b8e2add6f2539c0ddcb3e267b5
Lucas <lucas@agtlucas.com>
2016-01-02 07:58:16 -0200

    Update MainActivity

--- android/app/src/main/java/com/couve/MainActivity.java
+++ android/app/src/main/java/com/couve/MainActivity.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactRootView;
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.mapbox.reactnativemapboxgl.ReactNativeMapboxGLPackage;
 
 public class MainActivity extends Activity implements DefaultHardwareBackBtnHandler {
 
